9f0ce0e07647a61a4471a395896797a7d767dbb7
Sophie Reynolds <shreynolds101@gmail.com>
2020-07-08 11:03:37 -0700

    Added Weather API and tried to chain post request

--- website/app.js
+++ website/app.js
@@ -1,8 +1,45 @@
 /* Global Variables */
+link = 'http://api.openweathermap.org/data/2.5/weather?zip='
+key = '&appid=0d002347fe543ee21f35b26cc949411c'
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+let generate = document.getElementById('generate');
+let zipElement = document.getElementById('zip');
+let userElement = document.getElementById('feelings');
+
+generate.addEventListener('click', function(){
+    zip = zipElement.value;
+    console.log(zip);
+    zipElement.value = "";
+    getWeather(link, zip, key).then(function(data){
+        tempF = kelvinToFarenheit(data.main.temp);
+        console.log(tempF);
+        userFeelings = userElement.value;
+        postData('/add', {temp:tempF, date:newDate, feelings:userFeelings}) //issue here
+    });
+});
+
+
+function kelvinToFarenheit(tempK){
+    tempF = (tempK - 273.15) * 9/5 + 32;
+    return Math.round(tempF);
+}
+
+const getWeather = async(link, zip, key) =>{
+    const url = link+zip+key;
+    console.log(url);
+    const result = await fetch(url);
+    try{
+        const data = await result.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
 
 
 const retreiveData = async()=>{
@@ -10,10 +47,27 @@ const retreiveData = async()=>{
     console.log(url);
     const request = await fetch(url);
     try{
-        const allData = request.json();
+        const allData = await request.json();
     } catch(error) {
         console.log("error", error);
     }
 }
 
-retreiveData();
+
+const postData = async ( url = '', data = {})=>{
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),   
+  });
+
+    try {
+      const newData = await response.json();
+      return newData;
+    } catch(error) {
+    console.log("error", error);
+    }
+}
